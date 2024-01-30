430ebd0973a1805ca4833a7e516605543a820325
周建泽 <jianzecc@outlook.com>
2024-01-30 11:27:32 +0800

    1.gitignore 2.封装分页接口

---
+++ src/main/java/com/demo/pojo/entity/User.java
@@ -0,0 +1,131 @@
+package com.demo.pojo.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User implements Serializable {
+    /**
+     * 主键
+     */
+    @TableId(value = "id")
+    private String id;
+
+    /**
+     * 姓名
+     */
+    @TableField(value = "user_name")
+    private String userName;
+
+    /**
+     * 密码
+     */
+    @TableField(value = "password")
+    private String password;
+
+    /**
+     * 手机号码
+     */
+    @TableField(value = "phone")
+    private String phone;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "create_at",fill = FieldFill.INSERT)
+    private String createAt;
+
+    /**
+     * 创建人
+     */
+    @TableField(value = "create_by",fill = FieldFill.INSERT)
+    private String createBy;
+
+    /**
+     * 更新时间
+     */
+    @TableField(value = "update_at",fill = FieldFill.UPDATE)
+    private String updateAt;
+
+    /**
+     * 更新人
+     */
+    @TableField(value = "update_by",fill = FieldFill.UPDATE)
+    private String updateBy;
+
+    /**
+     * 假删除的标志
+     */
+    @TableField(value = "is_delete",fill =FieldFill.INSERT)
+    @TableLogic(value = "0",delval = "1")
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        User other = (User) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getUserName() == null ? other.getUserName() == null : this.getUserName().equals(other.getUserName()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()))
+            && (this.getCreateAt() == null ? other.getCreateAt() == null : this.getCreateAt().equals(other.getCreateAt()))
+            && (this.getCreateBy() == null ? other.getCreateBy() == null : this.getCreateBy().equals(other.getCreateBy()))
+            && (this.getUpdateAt() == null ? other.getUpdateAt() == null : this.getUpdateAt().equals(other.getUpdateAt()))
+            && (this.getUpdateBy() == null ? other.getUpdateBy() == null : this.getUpdateBy().equals(other.getUpdateBy()))
+            && (this.getIsDelete() == null ? other.getIsDelete() == null : this.getIsDelete().equals(other.getIsDelete()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getUserName() == null) ? 0 : getUserName().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
+        result = prime * result + ((getCreateAt() == null) ? 0 : getCreateAt().hashCode());
+        result = prime * result + ((getCreateBy() == null) ? 0 : getCreateBy().hashCode());
+        result = prime * result + ((getUpdateAt() == null) ? 0 : getUpdateAt().hashCode());
+        result = prime * result + ((getUpdateBy() == null) ? 0 : getUpdateBy().hashCode());
+        result = prime * result + ((getIsDelete() == null) ? 0 : getIsDelete().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", userName=").append(userName);
+        sb.append(", password=").append(password);
+        sb.append(", phone=").append(phone);
+        sb.append(", createAt=").append(createAt);
+        sb.append(", createBy=").append(createBy);
+        sb.append(", updateAt=").append(updateAt);
+        sb.append(", updateBy=").append(updateBy);
+        sb.append(", hasDelete=").append(isDelete);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/demo/util/ContextUtil.java
@@ -0,0 +1,15 @@
+package com.demo.util;
+/**
+ * description 模拟spring上下文
+ * @author 周建泽
+ * @date 2024/1/26
+ */
+public class ContextUtil {
+    /**
+     * 获取当前登录用户id
+     * @return  返回默认人员
+     */
+    public static String getCurrentUserId(){
+        return "zhangsan";
+    }
+}
